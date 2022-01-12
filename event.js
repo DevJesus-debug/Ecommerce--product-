54619a46a69b4c9af220146363d63a2db4046da1
DevJesus-debug <jesusterrazasdev25@gmail.com>
2022-01-11 16:01:02 -0800

    first commit

---
+++ event.js
@@ -0,0 +1,21 @@
+//Images
+const imageCont = document.querySelectorAll(".img-slider img");
+const imgOne = document.querySelector(".image-1");
+const imgTwo = document.querySelector(".image-2");
+const imgThree = document.querySelector(".image-3");
+const imgFour = document.querySelector(".image-4");
+//Buttons
+const nextBtn = document.querySelector(".right-btn");
+const prevBtn = document.querySelector(".left-btn");
+const quantity = document.querySelector(".number");
+const minus = document.querySelector(".minus-btn");
+const plus = document.querySelector(".plus-btn");
+
+
+let counter = 0;
+
+
+
+
+
+
